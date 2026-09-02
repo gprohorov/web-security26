8d68ea1fdc85d097465415d3b79b3a5ad9edc18d
gprohorov <g.prokhorov@indevlab.com>
2026-09-02 21:03:15 +0300

    initial commit

---
+++ src/main/java/chnu/edu/websecurity26/WebSecurity26Application.java
@@ -0,0 +1,13 @@
+package chnu.edu.websecurity26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecurity26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSecurity26Application.class, args);
+    }
+
+}
